ad335c1202187392017dc6df8175580627330f4c
Hazem-Gamall <hazemgamalelgendy@gmail.com>
2022-10-01 21:03:47 +0200

    Requests are now performed asynchronosly

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from time import sleep
 import requests
+import aiohttp
+import asyncio
 
 
-def get_session():
-    session = requests.Session()
-    session.get('https://register.must.edu.eg/StudentRegistrationSsb/ssb/registration')
+async def init_session(session):
+    await session.get('https://register.must.edu.eg/StudentRegistrationSsb/ssb/registration')
     return session
 
-def enable_search(session):
+async def enable_search(session):
     """My current theory is that i need to do this request so that
     the server assings something to my session in its database that allows me
     to perform the search request"""
@@ -25,7 +26,7 @@ def enable_search(session):
         # 'uniqueSessionId': 'wz0061664464778935',
     }
 
-    session.post(
+    await session.post(
         'https://register.must.edu.eg/StudentRegistrationSsb/ssb/term/search',
         params=params,
         headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
@@ -33,9 +34,7 @@ def enable_search(session):
         )
 
 
-def search(subject, course_number, crn_to_monitor):
-    session = get_session()
-    enable_search(session)
+async def search(subject, course_number, crns_to_monitor, session):
     
     course_search_params = {
         'txt_subject':subject,
@@ -47,32 +46,45 @@ def search(subject, course_number, crn_to_monitor):
         'sortColumn':'subjectDescription',
         'sortDirection':'asc'
     }
-    resp = session.get('https://register.must.edu.eg/StudentRegistrationSsb/ssb/searchResults/searchResults',params=course_search_params)
-    print(resp)
-    if resp.json()['data']:
-        for course_entry in resp.json()['data']:
-            crn = course_entry['meetingsFaculty'][0]['courseReferenceNumber']
-            seats_available = course_entry['seatsAvailable']
-            # print(f"Title: {course_entry['courseTitle']}")
-            # print(f"Type: {course_entry['scheduleTypeDescription']}")
-            # print(f"CRN: {crn}")
-            # print(f"seatsAvailable: {seats_available}")
-            # print('\n')
-            if crn == crn_to_monitor and seats_available > 0:
-                print("SEATS AVAILABLE MY NIGGA!!!\n\n")
-    else:
-        raise Exception('Please make sure that the course title you entered is correct.')
+    async with session.get('https://register.must.edu.eg/StudentRegistrationSsb/ssb/searchResults/searchResults',params=course_search_params) as response:
+        json_response = await response.json()
+        data = json_response['data']
+        if data:
+            for course_entry in data:
+                course_title = course_entry['courseTitle']
+                crn = course_entry['meetingsFaculty'][0]['courseReferenceNumber']
+                seats_available = course_entry['seatsAvailable']
 
-if __name__ == '__main__':
-    print("""Enter the course name without spaces""")
-    course_name = ''.join(input('course subject:').upper().split())
-    subject = course_name[:4]
-    course_number = course_name[4:]
-    crn_to_monitor = input("Enter the CRN you would like to monitor:")
-    print(subject, course_number, crn_to_monitor)
-    while True:
+                if crn in crns_to_monitor and seats_available > 0:
+                    print(f"{course_title}: {seats_available} SEATS AVAILABLE FOR {crn}\n\n")
+        else:
+            raise Exception('Please make sure that the course title you entered is correct.')
+
+async def main():
+    num_of_courses = int(input('How many courses do you want to track? '))
+    courses = []
+    for _ in range(num_of_courses):
+        print("""Enter the course name without spaces""")
+        course_name = ''.join(input('course subject:').upper().split())    
+        course = {
+            'subject': course_name[:4],
+            'course_number':course_name[4:],
+            'crns_to_monitor': input("Enter the CRNs you would like to monitor comma-seperated:").strip().split(',')
+        }
+         
+        courses.append(course)
+    async with aiohttp.ClientSession() as session:
+        await init_session(session)
+        await enable_search(session)
+        
+        
         try:
-            search(subject, course_number, crn_to_monitor)
+            while True:
+                tasks = [search(session=session, **course) for course in courses ]
+                await asyncio.gather(*tasks)
+                sleep(2)
         except Exception as e:
             print('Error', e)
-        sleep(60)
+
+if __name__ == '__main__':
+    asyncio.run(main())
